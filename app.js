d89fc93775d4dedcfa46f4f3e2c83f5271154ac1
Muzammil803 <syedmuzammil691@gmail.com>
2022-08-03 11:41:24 +0500

    complete

---
+++ app.js
@@ -0,0 +1,192 @@
+
+
+// //////////////////////////////////////////////////
+                // CHAANGE COUNTER
+// // //////////////////////////////////////////////////
+// var hundred=0
+// var fifty=0
+// var ten = 0
+// var input = +prompt("enter pkr")
+
+// for (var i = input; i>=100 ; i=input){
+    
+//         input=i-100;
+//         hundred = ++hundred
+    
+    
+    
+// }
+
+// for (var i = input; i>=50 ; i=input){
+    
+//     input=i-50;
+//     fifty = ++fifty
+
+
+
+// }
+
+// for (var i = input; i>=10 ; i=input){
+    
+//     input=i-10;
+//     ten = ++ten
+
+
+
+// }
+// console.log(hundred)
+// console.log(fifty)
+// console.log(ten)
+// alert("you have to return " + hundred+" hundred notes " +fifty+" fifty notes and "+ten+" ten notes")
+
+
+
+ 
+
+
+// ////////////////////////////////////////////////////
+// function calc(b,a){
+// var ans= a=a+b
+// alert(ans)
+
+
+// }
+// calc(3,2)
+
+// ////////////////////////////////////////////////
+
+            //    LEAP YEAR CHECKER
+
+// // //////////////////////////////////////////
+// input = prompt("enter a year to it is leap year or not")
+
+// var a = new Date("feb 29 "+input)
+// // console.log(a.getDate())
+// var leap = a.getDate()
+// if(leap===29){
+//     alert("its a leap year")
+// }else{
+//     alert ("its not a leap year")
+// }
+
+// /////////////////////////////////////////////////
+        //    AREA OF TRIANGLE
+        // /////////////////////////////////////
+
+
+// function area(a,b,c){
+//     var s = (a+b+c)/2
+//     var area=s*(s-a)*(s-b)*(s-c)
+//     alert("Area of triangle is "+area)
+// }
+// area(5,3,4)
+
+// //////////////////////////////////////////////////////
+
+// var a=87
+// var b=99
+// var c=79
+
+// function average(){
+//     var average=(a+b+c)/3
+//     return average;
+// }
+// function percentage(){
+//    var percentage= ((a+b+c)/300)*100
+// return percentage;
+// }
+// function result(){
+//     alert("the average of given number is "+ average().toFixed(2)+ " percentage is "+ percentage().toFixed(2))
+// }
+// result()
+// //////////////////////////////////////////////////////
+
+// var input = +prompt("enter distance b/w two cities in km")
+// function meter(){
+//     alert(input*1000 +" m")
+// }
+
+// function feet(){
+//     alert(input*3280.84+" ft")
+// }
+
+// function inches(){
+//     alert(input*3280.84*12+" inch")
+// }
+
+// function cm(){
+//     alert(input*1000*100+" cm")
+// }
+// meter()
+// feet()
+// inches()
+// cm()
+
+// ////////////////////////////////////////////////////////
+// var hour = +prompt("enter working hours")
+// function overpay(){
+// if (hour>40){
+//    var payment = (hour-40)*12 
+//    alert("your overtime payment is "+payment +" RS")
+// }
+
+// }
+// overpay()
+
+
+// /////////////////////////////////////////////////
+
+// var input=prompt("enter a name to find index ")
+// var arr = ["ali","muzammil","hammad","hamza","ghous"]
+// function indexof(){
+// for(i=0; i<arr.length; i++){
+//     if(input===arr[i]){
+//         alert(i)
+//     }
+    
+// }
+// if (input!=arr[i]){
+//     alert(-1)
+// }
+// }
+//  indexof()
+
+// /////////////////////////////////////////////////////////
+
+
+
+// var para = "my name is muzammil"
+// var arr = para.split("")
+ 
+// function removevovel(){
+//     for(var i =0; i<arr.length; i++){
+//         if(arr[i]==="a"||arr[i]==="e"||arr[i]==="i"||arr[i]==="o"||arr[i]==="u"){
+//           arr.splice(i,1)  
+//         }
+//     }
+// para = arr.join("")
+
+//     alert (para)
+// }
+
+// removevovel()
+
+
+
+// var para = "my name is muzammil"
+// var arr = para.split("")
+//  var a =""
+// function findvovel(){
+//     for(var i =0; i<arr.length; i++){
+//         if(arr[i]==="a"||arr[i]==="e"||arr[i]==="i"||arr[i]==="o"||arr[i]==="u"){
+//        var   ans=arr.slice(i,i+1)+ans
+      
+//       }
+//     }
+// //  var para =  arr.join("")
+//     console.log (ans)
+// }
+
+// findvovel()
+
+
